14c4ca32ce563dcbc2c49ed6048fab6a8c492528
rnaveee <152345538+rnaveee@users.noreply.github.com>
2023-12-08 17:54:22 -0800

    Delete Module 1 Assignment directory

--- Module 1 Assignment/src/com/ryan/module13/Rotate.java
+++
@@ -1,7 +0,0 @@
-package com.ryan.module13;
-
-public interface Rotate {
-    void rotate90();
-    void rotate180();
-    void rotate(double degree);
-}
--- Module 1 Assignment/src/com/ryan/module17/Rotate.java
+++
@@ -1,9 +0,0 @@
-package com.ryan.module17;
-
-public interface Rotate {
-
-    //rotation methods
-    void rotate90();
-    void rotate180();
-    void rotate(double degree);
-}
